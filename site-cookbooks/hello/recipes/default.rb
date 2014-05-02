11c62216ebe9f2804bfbd46d2926d39302e9044f
phuc le <phuclevn@gmail.com>
2014-05-02 09:40:26 +0000

    first commit, git and mercurial

---
+++ site-cookbooks/hello/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: hello
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+print "recipe was created by Phuc Q. Le\n"
+sudo apt-get install git
+
---
+++ site-cookbooks/mercurial/recipes/default.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: mercurial
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "mercurial" do
+  action :install
+end
